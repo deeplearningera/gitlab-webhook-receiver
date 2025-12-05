704d5172e64f777b8288ba5ac96b6e3975567ab7
Rachit Sharma <deeplearningera@gmail.com>
2025-12-05 21:13:39 +0530

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const bodyParser = require("body-parser");
+const routes = require("./routes");
 require("dotenv").config();
 const routes = require("./routes");
 
