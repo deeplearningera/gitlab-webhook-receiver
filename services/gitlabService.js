559912ec1a6f1b9397ec0dff9ca8329fb77b77b0
Rachit Sharma <deeplearningera@gmail.com>
2025-12-05 22:48:52 +0530

    Add files via upload

--- services/gitlabService.js
+++ services/gitlabService.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
 
 const gitlabApi = axios.create({
-  baseURL: "https://gitlab.payu.in/api/v4",
+  baseURL: "https://gitlab.in/api/v4",
   headers: {
     "PRIVATE-TOKEN": process.env.GITLAB_TOKEN,
   },
@@ -9,9 +9,11 @@ const gitlabApi = axios.create({
 
 exports.fetchMRChanges = async (projectId, mrIid) => {
   try {
+    console.log("Fetching gitlab diff");
     const response = await gitlabApi.get(
       `/projects/${projectId}/merge_requests/${mrIid}/changes`
     );
+    console.log("Fetched gitlab diff");
     return response.data.changes;
   } catch (err) {
     console.error("❌ GitLab API error:", err.response?.data || err);
