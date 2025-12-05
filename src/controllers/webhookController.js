88de265f10f3e95e608b5b6b32f76977ddfa4a43
Rachit Sharma <deeplearningera@gmail.com>
2025-12-05 21:04:09 +0530

    Add files via upload

---
+++ src/controllers/webhookController.js
@@ -0,0 +1,47 @@
+const gitlabService = require("../services/gitlabService");
+const queueService = require("../services/queueService");
+
+exports.handleWebhook = async (req, res) => {
+  try {
+    const gitlabToken = req.headers["x-gitlab-token"];
+    if (gitlabToken !== process.env.WEBHOOK_SECRET) {
+      return res.status(403).json({ error: "Invalid Webhook Token" });
+    }
+
+    const event = req.body;
+
+    // We only care about merged merge requests into master
+    if (
+      event.object_kind === "merge_request" &&
+      event.object_attributes.state === "merged" &&
+      event.object_attributes.target_branch === "master"
+    ) {
+      console.log("🔔 MR merged! Fetching diff...");
+
+      const projectId = event.project.id;
+      const mrIid = event.object_attributes.iid;
+
+      // Fetch full diff
+      const changes = await gitlabService.fetchMRChanges(projectId, mrIid);
+
+      const message = {
+        mr_id: mrIid,
+        project_id: projectId,
+        title: event.object_attributes.title,
+        description: event.object_attributes.description,
+        merged_by: event.user.name,
+        merged_at: event.object_attributes.updated_at,
+        diff: changes,
+        repo: event.project.path_with_namespace,
+      };
+
+      await queueService.sendToQueue(message);
+    }
+
+    return res.status(200).json({ status: "ok" });
+  } catch (err) {
+    console.error("❌ Error in webhook:", err);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
