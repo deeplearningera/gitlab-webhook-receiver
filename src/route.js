b1ccced21aee1fcc9c1fa90dc6129b2e1223ba6b
Rachit Sharma <deeplearningera@gmail.com>
2025-12-05 22:53:20 +0530

    Delete src/route.js

--- src/route.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const webhookController = require("./controllers/webhookController");
-
-router.post("/gitlab/webhook", webhookController.handleWebhook);
-
-module.exports = router;
-
